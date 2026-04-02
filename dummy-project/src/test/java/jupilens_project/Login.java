3060607e1214254d4fe7b542c96b9b7a77bf2c41
saksham <saksham@LAPTOP-TUQ1H1R3>
2026-04-02 11:48:52 +0530

    updated

--- dummy-project/src/test/java/jupilens_project/Login.java
+++ dummy-project/src/test/java/jupilens_project/Login.java
@@ -15,16 +15,21 @@ public static void main(String[] args) throws InterruptedException {
 	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 	
 	driver.get("https://jupilens.com/");
-	
+	Thread.sleep(2000);
 	Actions act = new Actions(driver);
 	
 	WebElement hoverTarget = driver.findElement(By.xpath("//button[contains(text(), 'Sign In')]"));
 	act.moveToElement(hoverTarget).build().perform();
+	Thread.sleep(3000);
 	
-	WebElement loginTalent = driver.findElement(By.xpath("//text() = 'Login as Talent'"));
+	WebElement loginTalent = driver.findElement(By.xpath("//button[contains(text(), 'Login as Talent')]"));
+	loginTalent.click();
+	Thread.sleep(3000);
 	
 	driver.findElement(By.id("email")).sendKeys("ankitiwari169@gmail.com");
+	Thread.sleep(3000);
 	driver.findElement(By.id("password")).sendKeys("Pass@543213");
+	Thread.sleep(3000);
 	
 	WebElement signnin = driver.findElement(By.xpath("//button[@type='submit']"));
 	signnin.click();
